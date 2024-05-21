bbc8b06df72fd52d7e3fc8a6fa477be08e1fe904
hcnc <hcnc@DESKTOP-IVFQGQ4>
2024-05-21 15:14:56 +0900

    05-21 김진우 음식 페이지 업데이트

---
+++ src/main/java/bookcafe/food/service/FoodVO.java
@@ -0,0 +1,45 @@
+package bookcafe.food.service;
+
+public class FoodVO {
+	private String product_code;
+	private String product_name;
+	private String product_price;
+	private String product_quantity;
+	private String product_img;
+	
+	
+	public String getProduct_code() {
+		return product_code;
+	}
+	public void setProduct_code(String product_code) {
+		this.product_code = product_code;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(String product_price) {
+		this.product_price = product_price;
+	}
+	public String getProduct_quantity() {
+		return product_quantity;
+	}
+	public void setProduct_quantity(String product_quantity) {
+		this.product_quantity = product_quantity;
+	}
+	public String getProduct_img() {
+		return product_img;
+	}
+	public void setProduct_img(String product_img) {
+		this.product_img = product_img;
+	}
+	
+	
+	
+	
+}

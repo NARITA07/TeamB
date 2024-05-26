7c6670276527ea1d7b3c7f1fab682d6754e0bbea
koo <worntjd142@naver.com>
2024-05-27 00:01:39 +0900

    a

---
+++ src/main/java/bookcafe/nexacro/productmanagement/service/ProductManagementService.java
@@ -0,0 +1,23 @@
+package bookcafe.nexacro.productmanagement.service;
+
+import java.util.Map;
+
+public interface ProductManagementService {
+	
+	//음식 이미지 등록
+	void Food_Product(String path);
+	
+	//도서 이미지 등록
+	void BooK_Product(String path);
+	
+	//음식 등록
+	int food_insert(Map<String, String> pM);
+	
+	//도서 등록
+	int book_insert(Map<String, String> pM);
+
+
+
+
+
+}
---
+++ src/main/java/bookcafe/nexacro/productmanagement/serviceImpl/ProductManagementServiceImpl.java
@@ -0,0 +1,49 @@
+package bookcafe.nexacro.productmanagement.serviceImpl;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import bookcafe.nexacro.productmanagement.service.ProductManagementService;
+import egovframework.rte.fdl.cmmn.EgovAbstractServiceImpl;
+
+@Service
+public class ProductManagementServiceImpl extends EgovAbstractServiceImpl implements ProductManagementService{
+
+	@Autowired
+	ProductManagementMapper pmm;
+	
+	//음식 이미지 등록
+	@Override
+	public void Food_Product(String path) {
+		pmm.Food_Product(path);
+	}
+	
+	//도서 이미지 등록
+	@Override
+	public void BooK_Product(String path) {
+		pmm.BooK_Product(path);
+		
+	}
+	
+	
+	
+	//음식 등록
+	@Override
+	public int food_insert(Map<String, String> pM) {
+		return  pmm.food_insert(pM);
+		
+	}
+	
+	//도서 등록
+	@Override
+	public int book_insert(Map<String, String> pM) {
+		
+		return pmm.book_insert(pM);
+	}
+
+
+
+
+}

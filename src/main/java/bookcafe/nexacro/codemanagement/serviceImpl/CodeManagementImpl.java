9d54ef090ad53b2cdfe8d2cf37227fba167cb25b
koo <worntjd142@naver.com>
2024-06-11 22:33:07 +0900

    코드관리 중

--- src/main/java/bookcafe/nexacro/codemanagement/serviceImpl/CodeManagementImpl.java
+++ src/main/java/bookcafe/nexacro/codemanagement/serviceImpl/CodeManagementImpl.java
@@ -15,112 +15,17 @@ public class CodeManagementImpl implements CodeManagementService {
 
 	@Autowired
 	CodeManagementMapper codemmapper;
-	
-	//대분류 조회
-	public List<Map<String, Object>> init_fir_code(){
 
-		List<Map<String, Object>> re_init_fir_code = codemmapper.init_fir_code();
+	//상위코드 그리드 조회
+	public List<Map<String, Object>> selectfirgrid() {
 		
-		return re_init_fir_code;
-	}
-	
-	
-	@Override
-	public List<Map<String, Object>> sec_category_select(Map<String, Object> sec_category_select) {
-		List<Map<String, Object>> ssec_category_select = codemmapper.sec_category_select(sec_category_select);
-		return ssec_category_select;
+		return codemmapper.selectfirgrid();
 	}
 
-
-	@Override
-	public Map<String, Object> C_fir_code_chx(List<Map<String, Object>> modi_date) {
-		Map<String, Object> num = new HashMap<>();
-		int date = 0;
-		for(int i = 0; i < modi_date.size(); i++) {	
-
-			int result = codemmapper.select_mo_or_in(modi_date.get(i).get("FIR_CODE"));
-		if(result == 1) { // 값이 있음 -> 수정 
-			
-			//중위코드
-			date += codemmapper.C_sec_code_chx(modi_date.get(i));
-			
-			//상위코드
-			date += codemmapper.C_fir_code_chx(modi_date.get(i));
-			
-		
-			}else {// 그 외는 인서트
-			
-			
-				date += codemmapper.insert_fir_code(modi_date.get(i));
-			
-			}
-			num.put("num", date);
-		}
-		
-		return num;
+	//하위코드 그리드 조회
+	public List<Map<String, Object>> selectsecgrid(String fir_category) {
+		return codemmapper.selectsecgrid(fir_category);
 	}
-
-
-	//상위코드 생성
-	@Override
-	public Map<String, Object> add_fir_code(Map<String, Object>fir_code_add) {
-		
-		Map<String, Object> codes = new HashMap<>();
-		int num = codemmapper.add_fir_code(fir_code_add);
-		System.out.println(num);
-		codes.put("num", num);
-		return codes;
-		
-	}
-
-	//상위코드 삭제
-	@Override
-	public void del_date(List<Map<String, Object>> del_date) {
-		
-		for(int i = 0; i < del_date.size(); i++ ) {
-			
-			codemmapper.C_del_date(del_date.get(i));
-			codemmapper.C_del_sec_date(del_date.get(i));
-		}
-		
-		
-	}
-
-
-	@Override
-	public void sec_code_add(Map<String, Object> sec_code_add) {
-		codemmapper.sec_code_add(sec_code_add);
-		
-	}
-
-
-	@Override
-	public void sec_code_modi(List<Map<String, Object>> modi_date_sec) {
-		
-for(int i = 0; i < modi_date_sec.size(); i++ ) {
-	codemmapper.sec_code_modi(modi_date_sec.get(i));
-		}
-		
-	}
-
-
-	@Override
-	public void del_date_sec(List<Map<String, Object>> del_date_sec) {
-		
-	for(int i = 0; i < del_date_sec.size(); i++ ) {
-			
-			codemmapper.del_date_sec(del_date_sec.get(i));
-		}
-		
-		
-	}
-
-	
-	@Override
-	public String max_code() {
-		
-		return codemmapper.max_code();
-	}		
 	
 	
 

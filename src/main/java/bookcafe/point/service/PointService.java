1772174dc4b5c56bfb6812461661eb069930ff05
ceasot <hawjdtn2@naver.com>
2024-06-14 11:51:57 +0900

    주문현황 환불 기능 (포인트 로그 추가) 완료 체크박스, 주문상태 변경에 처리 완료

--- src/main/java/bookcafe/point/service/PointService.java
+++ src/main/java/bookcafe/point/service/PointService.java
@@ -1,6 +1,7 @@
 package bookcafe.point.service;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PointService {
 	
@@ -16,4 +17,6 @@ public interface PointService {
 	// 포인트내역 합산금액 조회
 	public int selectTotalPoint(String user_code);
 	
+	// 주문코드를 통해 포인트 내역 조회
+	public List<Map<String,Object>> selectOrderToPointLog(String order_code);
 }
